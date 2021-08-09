60b71d2725d15ac4dd265ff921a1943306674388
kose25 <carlosjosepg@ufps.edu.co>
2021-08-09 00:51:00 -0500

    script movido aparte

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(function () {
+    var maxL = 1000;
+    $('.content').each(function () {
+        var text = $(this).html();
+        if (text.length > maxL) {
+            var begin = text.substr(0, maxL),
+                end = text.substr(maxL);
+            $(this).html(begin)
+                //.append($('<a class="readmore"/>').attr('href', '#').html('read more...'))
+                .append($('<span class="hidden" />').html(end))
+                .after("<button type='button' class='btn btn-primary readmorexd'>MOSTRAR MAS</button>");
+
+        }
+    });
+    $(document).on('click', '.readmorexd', function () {
+        // $(this).next('.readmore').fadeOut("400");
+        $(this).text(function (i, text) {
+            return text === "MOSTRAR MAS" ? "MOSTRAR MENOS" : "MOSTRAR MAS";
+        });
+        //var add = $(this).prev().children('.hidden').text();
+        $(this).prev().children('.hidden').slideToggle(400);
+        //$(this).prev().append(add).html;
+        //console.log(add);
+    });
+    $('.location').each(function (index) {
+        $(this).attr("href", "#collapseExample" + index);
+        $(this).parent().parent().next().children().first().attr("id", "collapseExample" + index);
+    });
+    $('.hotel').each(function (index) {
+        $(this).attr("href", "#tarifa" + index);
+        $(this).parent().parent().next().children().eq(1).attr("id", "tarifa" + index);
+    });
+})
